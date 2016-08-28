9df0326a10c007b9eafb49332f428cca120beaf6
Gokula2010 <Gokula2010@yahoo.com>
2016-08-28 18:49:36 +0800

    bootstrap css and component.html added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: `
-    <h1>Component Router</h1>
-    <nav>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-      <a routerLink="/crisis-center">Crisis Center</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `
+    templateUrl:'app.component.html'
+  //   template: `
+  //   <h1>Component Router</h1>
+  //   <nav>
+  //     <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+  //     <a routerLink="/crisis-center">Crisis Center</a>
+  //   </nav>
+  //   <router-outlet></router-outlet>
+  // `
 })
 export class AppComponent { }
