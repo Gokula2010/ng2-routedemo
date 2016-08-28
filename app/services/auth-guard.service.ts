3c60143f97548f4a2c6f342d604ce6177c9053f3
Gokula2010 <Gokula2010@yahoo.com>
2016-08-28 18:54:26 +0800

    auth and auth-guard service added

---
+++ app/services/auth-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { CanActivate, Router }  from '@angular/router';
+
+import { AuthService } from './auth.service';
+
+@Injectable() 
+export class AuthGuardService implements CanActivate {
+    constructor(private _router: Router, private _authService: AuthService) {
+
+    }
+
+    canActivate() {
+        if(this._authService.isLoggedIn)
+            return true;
+
+        this._router.navigate(['/login']);
+        return false;
+    } 
+}
+
